1ff77fb0b4ca15955796ce5c93e48c65d9d8ea9e
mr-exception <darbandi1996@gmail.com>
2017-07-18 18:13:20 +0430

    mongoose installed for api server

--- api-server/server.js
+++ api-server/server.js
@@ -4,4 +4,4 @@ var express = require('express'),
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('anazli art portal API server started on: ' + port);
