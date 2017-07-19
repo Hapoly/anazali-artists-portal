a8fa7162cd8f1a18872ab3e2d365fc8f0f9ee4f4
mr-exception <darbandi1996@gmail.com>
2017-07-19 20:02:41 +0430

    register api backend completed

--- artist/src/server/auth.js
+++ artist/src/server/auth.js
@@ -11,6 +11,27 @@ var userCheck = (userName, password, callBack) => {
         callBack(false);
 }
 
+var userRegister = (data, callBack) => {
+    var data = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            if(this.body.result = 'sucess')
+                callBack(true, this.body.info);
+            else
+                callBack(false, this.body.errors);
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://localhost:5000/register");
+    xhr.setRequestHeader("content-type", "application/json");
+
+    xhr.send(data);
+}
 var userGetInformation = (userName) => {
     return false;
 }
