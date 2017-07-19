aed90da0a67f76630f59c6d91b143d963bae5c31
mr-exception <darbandi1996@gmail.com>
2017-07-19 20:47:32 +0430

    access controll commit

--- api-server/server.py
+++ api-server/server.py
@@ -7,20 +7,24 @@ from config import *
 app = Flask(__name__)
 
 @app.route("/")
+@crossdomain(origin='*')
 def index():
     return "this is api server version (1.0.0)"
 
 # auth : login and register apis
 @app.route("/register", methods=['POST'])
+@crossdomain(origin='*')
 def register():
     return json.dumps(auth.register(request.json, db))
 
 @app.route("/login", methods=['POST'])
+@crossdomain(origin='*')
 def login():
     return "this is athe login method"
 
 
 @app.route('/test-output', methods=['GET', 'POST'])
+@crossdomain(origin='*')
 def test_output():
     content = request.json
     return json.dumps(content)
