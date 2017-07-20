ef65f0d448f501927df8ef1abbdf36e2882f3801
mr-exception <darbandi1996@gmail.com>
2017-07-20 13:17:00 +0430

    server configuration

--- api-server/server.py
+++ api-server/server.py
@@ -1,30 +1,30 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import json
 
 # configuration file
 from config import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
-@crossdomain(origin='*')
+@cross_origin()
 def index():
     return "this is api server version (1.0.0)"
 
 # auth : login and register apis
 @app.route("/register", methods=['POST'])
-@crossdomain(origin='*')
+@cross_origin()
 def register():
     return json.dumps(auth.register(request.json, db))
 
 @app.route("/login", methods=['POST'])
-@crossdomain(origin='*')
 def login():
     return "this is athe login method"
 
 
 @app.route('/test-output', methods=['GET', 'POST'])
-@crossdomain(origin='*')
 def test_output():
     content = request.json
     return json.dumps(content)
