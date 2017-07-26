42479948e930a6523fab0e8c3bb9c41fd0f68eb7
mr-exception <darbandi1996@gmail.com>
2017-07-26 07:14:47 +0430

    routing polict completed

---
+++ artist/src/Pages/NoMatch.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NoMatch extends Component {
+	constructor(props) {
+		super(props);
+    }
+
+  /* this is the main script render for Auth componenet */
+  render() {
+    return (
+        <div>not found</div>
+    );
+  }
+}
+
+export default NoMatch;
--- artist/src/index.js
+++ artist/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter ,Switch } from 'react-router-dom'
 import Auth from './Auth.js';
 import Dashboard from './Dashboard.js';
 import Messages from './Messages.js';
@@ -8,24 +8,61 @@ import Artists from './Artists.js';
 import Events from './Events.js';
 import Artistdetail from './Artistdetail.js';
 import Reports from './Reports.js';
+import NoMatch from './Pages/NoMatch';
 
 import registerServiceWorker from './registerServiceWorker';
 /* cookies progress */
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
-const not_logged = () => {
-	if(cookies.get('email') !== undefined)
-		window.location = './dashboard';
+
+/* policy states */
+const logged_in = () => {
+	return(typeof cookies.get('email') !== 'undefined');
 }
+const is_admin = () => {
+	return cookies.get('user').type === 'ADMIN';
+}
+const PrivateRoute = ({ component: Component, hasPermission, redirect, ...rest }) => (
+  <Route {...rest} render={props => (
+    hasPermission ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: redirect,
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
 ReactDOM.render(
-		<Router history = {browserHistory}>
-			<Route path = "/" component = {Auth} onEnter={not_logged}/>
-			<Route path="/dashboard" component={Dashboard}/>
-			<Route path="/messages" component={Messages}/>
-			<Route path="/artists" component={Artists}/>
-			<Route path="/events" component={Events}/>
-			<Route path="/artistdetail" component={Artistdetail}/>
-			<Route path="/reports" component={Reports}/>
+		<Router>
+				{logged_in()?
+				<Switch>
+					<Route path="/dashboard" 
+						component={is_admin()?Dashboard:Dashboard} exact/>
+					<Route path="/messages" 
+						component={is_admin()?Messages:Messages} exact/>
+					<Route path="/artists" 
+						component={is_admin()?Artists:Artists} exact/>
+					<Route path="/events" 
+						component={is_admin()?Events:Events} exact/>
+					<Route path="/artistdetail" 
+						component={is_admin()?Artistdetail:Artistdetail} exact/>
+					<Route path="/reports" 
+						component={is_admin()?Reports:Reports} exact/>
+
+					<Redirect from="/login" to="/dashboard"/>
+					<Redirect from="/" to="/dashboard"/>
+
+					<Route component={NoMatch} exact/>
+				</Switch>
+				:
+				<Switch>
+					<Route path="/login" component={Auth}/>
+					<Redirect to="/login" />
+				</Switch>
+				}
 		</Router>
 	, document.getElementById('root'));
 registerServiceWorker();
