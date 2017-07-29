89b1ae7acb3d2d3dd220b5152aeec63b3753a642
mr-exception <darbandi1996@gmail.com>
2017-07-29 19:43:17 +0430

    some plugin install

--- api-server/image-uploader/index.js
+++ api-server/image-uploader/index.js
@@ -1,7 +1,18 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 9090;
+var express = require('express');
+var corsPrefetch = require('cors-prefetch-middleware');
+var imagesUpload = require('images-upload-middleware');
 
+app.use('/static', express.static('./server/static'));
+app.use(corsPrefetch);
+
+app = express();
+port = process.env.PORT || 9090;
+
+app.post('/notmultiple', imagesUpload(
+    './server/static/files',
+    'http://localhost:9090/static/files'
+));
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
+
